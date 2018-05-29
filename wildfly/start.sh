22ce01181cd4614e8486b7dd51d5abff9d71ff5e
Kelly Goedert <kelly@substractum.com.br>
2018-05-29 10:15:20 -0300

    driver module

---
+++ wildfly/start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm -rf /opt/jboss/wildfly/standalone/log
+rm -rf /opt/jboss/wildfly/standalone/tmp
+rm -rf /opt/jboss/wildfly/standalone/data
+/opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 -c standalone.xml --debug
