933996f843a63fbd441b98eba765c8be98b65a01
Kelly Goedert <kelly.goedert@gmail.com>
2018-06-06 13:40:47 -0300

    widlfly version update

--- wildfly/start.sh
+++ wildfly/start.sh
@@ -3,4 +3,4 @@
 rm -rf /opt/jboss/wildfly/standalone/log
 rm -rf /opt/jboss/wildfly/standalone/tmp
 rm -rf /opt/jboss/wildfly/standalone/data
-/opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 -c standalone.xml --debug
+/opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 -c standalone-ee8.xml --debug
